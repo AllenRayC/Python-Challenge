24b168d991be617419987a0f014a42546d2f791c
Allen Chang <raiyu5438@gmail.com>
2018-08-27 19:38:58 -0400

    finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,7 @@ import os
 import csv
 
 # Set path for file
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join("..", "PyBank","Resources", "budget_data.csv")
 
 # Set variables
 total_months = 0
